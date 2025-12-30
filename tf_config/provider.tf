e4f26b67e0da38245e910c7b0cded3469c677824
obermejo <obermejo@sdi.es>
2025-12-30 10:26:23 +0100

    try to solve the problem of version in terraform

--- tf_config/provider.tf
+++ tf_config/provider.tf
@@ -10,7 +10,7 @@ terraform {
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "2.0.1" # Latest version as March 2021 = 3.1.0. Using 2.0.1 (April, 2020) for ORM compatibility
+    
     }
     local = {
       source  = "hashicorp/local"
